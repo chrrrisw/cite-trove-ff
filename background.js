50eb491bd958aa7f73cc49e1104f183ea877b7a2
Chris Willoughby <chrrrisw@gmail.com>
2017-11-21 11:34:48 +1030

    WIP: Copied from chrome.

--- background.js
+++ background.js
@@ -1,34 +1,95 @@
-/* This Source Code Form is subject to the terms of the Mozilla Public
+/**
+ * This Source Code Form is subject to the terms of the Mozilla Public
  * License, v. 2.0. If a copy of the MPL was not distributed with this
- * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ *
+ * @author Chris Willoughby
+ */
 
-var self = require("sdk/self");
-var clipboard = require("sdk/clipboard");
-var contextMenu = require("sdk/context-menu");
-var preferences = require("sdk/simple-prefs");
-var tabs = require("sdk/tabs");
-var formatter = require("./lib/formatter.js");
+ "use strict";
 
-// Use the preferences to format the output, and place in clipboard.
-function copyCitation(citation) {
-    var format = preferences.prefs.formatCitation;
-    var copyText = formatter.formatCitation(format, citation);
-    clipboard.set(copyText);
+/**
+ * The callback function for the citeTrove menu item.
+ */
+function onClickHandler(info, tab) {
+    if (info.menuItemId == "citeTrove") {
+        console.log("citeTrove pressed");
+        // Must use chrome.tabs.sendMessage to send to content script.
+        chrome.tabs.sendMessage(
+            tab.id,
+            {type : "cite"});
+
+    } else {
+        console.log("Not citeTrove menu item");
+
+    }
 }
 
-preferences.on("showHelp", function() {
-    tabs.open(self.data.url("cite_trove.html"));
+/**
+ * Add a listener for clicks in the context menu.
+ */
+chrome.contextMenus.onClicked.addListener(onClickHandler);
+
+/**
+ * Get the format from storage, format the citation, put it in selection.
+ */
+function formatAndCopy(message) {
+    chrome.storage.sync.get({
+        troveFormat: "%A%n%C%n%T%n%I, page %P%n[Quote]%n%Q%n[Quote]%n"
+      }, function(items) {
+        var input = document.createElement("textarea");
+        document.body.appendChild(input);
+        input.value = formatCitation(items.troveFormat, message);
+        input.focus();
+        input.select();
+        document.execCommand("Copy");
+        input.remove();
+
+      });
+}
+
+/**
+ * Add a listener for messages from the content script.
+ */
+chrome.runtime.onMessage.addListener(function(message) {
+    if (message && message.type == "citation") {
+        formatAndCopy(message);
+    }
 });
 
-// Create a context menu item, only on Trove pages with selected text.
-var citeTroveMenuItem = contextMenu.Item({
-    label : "Cite Trove",
-    context : [ contextMenu.URLContext("*.trove.nla.gov.au"),
-            contextMenu.SelectionContext() ],
-    contentScriptFile : [
-        self.data.url("scraper.js"),
-        self.data.url("trove_select.js")],
-    contentScript : "self.on('context', cite_trove_context_cb);"
-            + "self.on('click', cite_trove_click_cb);",
-    onMessage : copyCitation
+/**
+ * A rule to show the pageAction on the trove.nla.gov.au site.
+ */
+var pageActionRule = {
+    conditions: [
+        new chrome.declarativeContent.PageStateMatcher({
+            pageUrl: { hostEquals: "trove.nla.gov.au", schemes: ["http", "https"] }
+        })
+    ],
+    actions: [ new chrome.declarativeContent.ShowPageAction() ]
+};
+
+/**
+ * When installed set up context menu item and add a rule to show pageAction.
+ */
+chrome.runtime.onInstalled.addListener(function(details) {
+    chrome.contextMenus.create({
+        "title" : "Cite Trove",
+        "contexts" : ["selection"],
+        "id" : "citeTrove",
+        "documentUrlPatterns": ["http://trove.nla.gov.au/*", "http://trove-beta.nla.gov.au/*"]
+    });
+
+    chrome.declarativeContent.onPageChanged.removeRules(undefined, function() {
+        chrome.declarativeContent.onPageChanged.addRules([pageActionRule]);
+    });
 });
+
+function pageActionCallback(tab) {
+    // console.log("pageAction pressed");
+    chrome.tabs.sendMessage(
+        tab.id,
+        {type : "cite"});
+}
+
+chrome.pageAction.onClicked.addListener(pageActionCallback);
